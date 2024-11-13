b5fac017e160dc11588d77d624b1a9070eebbd50
JARP <andersonandy046@gmail.com>
2024-11-14 00:31:52 +0800

    Uodate Sql

--- db/parking_portal.sql
+++ db/parking_portal.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 10, 2024 at 08:12 PM
+-- Generation Time: Nov 13, 2024 at 05:31 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -20,6 +20,30 @@ SET time_zone = "+00:00";
 --
 -- Database: `parking_portal`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cars`
+--
+
+CREATE TABLE `cars` (
+  `car_id` int(11) NOT NULL,
+  `carName` varchar(60) NOT NULL,
+  `carType` varchar(60) NOT NULL,
+  `plateNumber` varchar(60) NOT NULL,
+  `condo` varchar(60) NOT NULL,
+  `RFID` varchar(60) NOT NULL,
+  `CarImage` varchar(255) NOT NULL,
+  `carStatus` int(11) NOT NULL DEFAULT 1 COMMENT '0=archive, 1=active'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `cars`
+--
+
+INSERT INTO `cars` (`car_id`, `carName`, `carType`, `plateNumber`, `condo`, `RFID`, `CarImage`, `carStatus`) VALUES
+(4, 'Juan Delacruz', 'A', '00001', '12345', '012414', 'eeed21bb-f24c-4b7e-a593-4eca9b1d34fa.jpeg', 1);
 
 -- --------------------------------------------------------
 
@@ -45,6 +69,12 @@ INSERT INTO `users` (`id`, `username`, `password`) VALUES
 --
 
 --
+-- Indexes for table `cars`
+--
+ALTER TABLE `cars`
+  ADD PRIMARY KEY (`car_id`);
+
+--
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
@@ -53,6 +83,12 @@ ALTER TABLE `users`
 --
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `cars`
+--
+ALTER TABLE `cars`
+  MODIFY `car_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `users`
